379500f65d7ab764879220cd17e8f9bc1fcc6d80
enricodellamonica <enricodellamonica@gmail.com>
2013-04-09 22:58:44 +0200

    sign in page styled

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+
+  has_many :statuses
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -8,4 +10,8 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :first_name,:last_name, :profile_name,:email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  def full_name
+    first_name + " " + last_name
+  end
 end
